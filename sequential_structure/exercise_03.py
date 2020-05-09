ab3141091228c7d3ee5355e49f64801d6977b59a
huidemar <huidemar@gmail.com>
2020-05-08 23:41:08 -0300

    exercise 03 Faça um Programa que peça dois números e imprima a soma.
    close #3

---
+++ sequential_structure/exercise_03.py
@@ -0,0 +1,14 @@
+numero_1 = input('Digite um número: ')
+numero_2 = int(input('Digite outro número: '))
+
+numero_1 = int(numero_1)
+
+soma = numero_1 + numero_2
+
+print(f'A soma é {soma}')
+
+
+
+outra_soma = sum([numero_1, numero_2])
+
+print(f'a outra soma é {outra_soma}')
